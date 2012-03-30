b9cbe5fc3fed9c2675a1f1a519d76232c7f94f2e
Jing Yang <ditsing@gmail.com>
2012-03-30 12:27:48 +0800

    Update .c runable under windows.

--- fs/hello.c
+++ fs/hello.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "tslib/read_super.h"
+#include "ntddk.h"
 
 int main()
 {
-	init();
 	char magic[100];
+	init();
+
 	get_magic( magic);
 	printf("%s\n", magic);
 	return 0;
+}
+
+NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject,PUNICODE_STRING
+RegistryPath)
+{
+	main();
+return STATUS_UNSUCCESSFUL;
 }
