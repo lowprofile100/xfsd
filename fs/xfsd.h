ef88d1fc2b6a81d3f66221e64e40216d3c47ed66
Jing Yang <ditsing@gmail.com>
2012-06-19 20:39:30 +0800

    Half support to read a file by full path.

---
+++ fs/xfsd.h
@@ -0,0 +1,13 @@
+#ifndef __XFSD_H__
+#define __XFSD_H__
+
+/*
+ * Copied from include/linux/stat.h
+ */
+#define S_IFLNK	 0120000
+#define S_IFREG  0100000
+#define S_IFDIR  0040000
+#define S_IFMT  00170000
+
+#endif
+
