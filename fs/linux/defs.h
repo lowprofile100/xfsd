9f8437b039b5ecec1c3bb619739360aa7251be86
Jing Yang <ditsing@gmail.com>
2012-06-24 10:01:28 +0800

    Do not need to dealing with __attribute__.

--- fs/linux/defs.h
+++ fs/linux/defs.h
@@ -1,13 +1,9 @@
 #ifndef __LINUX_DEFS_H__
 #define __LINUX_DEFS_H__
 
-#ifdef WIN32
-#define __attribute__(x)
-#endif
 #define EXPORT_SYMBOL(x)
 #if defined(__cplusplus)
 #define NULL 0
 #else
 #define NULL (( void *)0)
-#endif
 #endif
